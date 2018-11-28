6a3548d566973e6860b91cea82503886127ad89c
Alessandro Mazzon <broodyblack@hotmail.it>
2018-11-28 15:45:30 +0100

    First commit

---
+++ app/src/main/java/printer/ev3printer/MainActivity.java
@@ -0,0 +1,15 @@
+package printer.ev3printer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import it.unive.dais.legodroid.lib.plugs.TachoMotor;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
