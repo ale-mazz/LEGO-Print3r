aee2e4fe5761ab946095c2e7721ee27bd097f088
Alessandro Mazzon <broodyblack@hotmail.it>
2018-12-03 11:50:07 +0100

    Bottons added (not working) BT connection added (not working)

--- app/src/main/java/printer/ev3printer/MainActivity.java
+++ app/src/main/java/printer/ev3printer/MainActivity.java
@@ -2,16 +2,51 @@ package printer.ev3printer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
+import java.io.IOException;
+
+import it.unive.dais.legodroid.lib.EV3;
+import it.unive.dais.legodroid.lib.comm.BluetoothConnection;
 import it.unive.dais.legodroid.lib.plugs.TachoMotor;
+import it.unive.dais.legodroid.lib.util.Prelude;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = Prelude.ReTAG("MainActivity");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+
+            // Connect to EV3 (HAL9000) via Bluetooth
+
+            EV3 ev3 = new EV3(new BluetoothConnection("HAL9000").connect());
+
+            Button startButton = findViewById(R.id.startButton);
+            startButton.setOnClickListener(v -> Prelude.trap(() -> ev3.run(this::legoMain)));
+
+
+            //TODO: fare un listener per i bottoni start/stop
+
+            Button stopButton = findViewById(R.id.stopButton);
+            stopButton.setOnClickListener(v -> {
+                ev3.cancel();   // Fire cancellation signal to the EV3 task
+            });
+
+        } catch (IOException e) {
+            Log.e(TAG, "Fatal error: cannot connect to HAL9000");
+            e.printStackTrace();
+        }
     }
 
-    //Commento di test
+    private void legoMain(EV3.Api api) {
+
+        //TODO: far fare qualcosa ai bottoni, questo è il "main" effettivo dell'applicazione
+
+    }
 }
