a462a846d54c978a00b4b1d28f8121a173a19d81
tomzeta <tommy.zambon@gmail.com>
2018-12-04 16:49:49 +0100

    Testing failed

--- app/src/main/java/printer/ev3printer/MainActivity.java
+++ app/src/main/java/printer/ev3printer/MainActivity.java
@@ -1,15 +1,38 @@
 package printer.ev3printer;
 
+import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import it.unive.dais.legodroid.lib.EV3;
+import it.unive.dais.legodroid.lib.comm.BluetoothConnection;
+import it.unive.dais.legodroid.lib.plugs.Plug;
 import it.unive.dais.legodroid.lib.plugs.TachoMotor;
+import it.unive.dais.legodroid.lib.util.Consumer;
+import it.unive.dais.legodroid.lib.util.Prelude;
+import it.unive.dais.legodroid.lib.util.ThrowingConsumer;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
     }
 }
