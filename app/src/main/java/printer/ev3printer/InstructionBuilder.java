5d9889833c83fef7ab6d5c9b5787595c62af128d
Simone Romanello <rommyalvise@hotmail.it>
2018-12-29 13:07:20 +0100

    Snake pattern printing added

--- app/src/main/java/printer/ev3printer/InstructionBuilder.java
+++ app/src/main/java/printer/ev3printer/InstructionBuilder.java
@@ -8,33 +8,63 @@ public class InstructionBuilder {
     public static ArrayList<PrinterInstruction> BuildInstructionListFromBitmap(boolean[][] array, int width, int height){
         ArrayList<PrinterInstruction> list = new ArrayList<PrinterInstruction>();
         int emptyLeft = 0;
+        int emptyRight = 0;
         int rightAmount = 0;
+        int leftAmount = 0;
         boolean amIADot = false;
-        for (int y = 0; y<height; y++){
-            for (int x = 0; x < width; x++){
-                if(array[y][x] == true){
-                    amIADot = true;
+        for (int y = 0; y<height; y++) {
+            if (y % 2 == 0) {
+                for (int x = 0; x < width; x++) {
+                    if (array[y][x] == true) {
+                        amIADot = true;
+                    }
+                    if (emptyLeft > 0 && amIADot) {
+                        list.add(new PrinterInstruction(PrinterInstruction.Direction.RIGHT, emptyLeft));
+                        rightAmount += emptyLeft;
+                        emptyLeft = 0;
+                    }
+                    if (amIADot) {
+                        list.add(new PrinterInstruction(PrinterInstruction.Direction.POINT, 1));
+                        emptyLeft++;
+                        amIADot = false;
+                    } else {
+                        emptyLeft++;
+                    }
                 }
-                if(emptyLeft > 0 && amIADot){
-                    list.add(new PrinterInstruction(PrinterInstruction.Direction.RIGHT, emptyLeft));
-                    rightAmount+= emptyLeft;
-                    emptyLeft = 0;
+                //devo tornare indietro di rightAmount
+                /*if (rightAmount > 0) {
+                    list.add(new PrinterInstruction(PrinterInstruction.Direction.LEFT, rightAmount));
+                    rightAmount = 0;
                 }
-                if(amIADot){
-                    list.add(new PrinterInstruction(PrinterInstruction.Direction.POINT, 1));
-                    emptyLeft ++;
-                    amIADot = false;
-                } else {
-                    emptyLeft++;
-                }
+                emptyLeft = 0;
+                list.add(new PrinterInstruction(PrinterInstruction.Direction.FORWARD, 1));*/
             }
-            //devo tornare indietro di rightAmount
-            if(rightAmount > 0) {
-                list.add(new PrinterInstruction(PrinterInstruction.Direction.LEFT, rightAmount));
-                rightAmount = 0;
+            else{
+                for (int x = width - 1; x >= 0; x--) {
+                    if (array[y][x] == true) {
+                        amIADot = true;
+                    }
+                    if (emptyRight > 0 && amIADot) {
+                        list.add(new PrinterInstruction(PrinterInstruction.Direction.LEFT, emptyRight));
+                        leftAmount += emptyRight;
+                        emptyRight = 0;
+                    }
+                    if (amIADot) {
+                        list.add(new PrinterInstruction(PrinterInstruction.Direction.POINT, 1));
+                        emptyRight++;
+                        amIADot = false;
+                    } else {
+                        emptyRight++;
+                    }
+                }
+                //devo tornare indietro di rightAmount
+                /*if (rightAmount > 0) {
+                    list.add(new PrinterInstruction(PrinterInstruction.Direction.LEFT, rightAmount));
+                    rightAmount = 0;
+                }
+                emptyLeft = 0;
+                list.add(new PrinterInstruction(PrinterInstruction.Direction.FORWARD, 1));*/
             }
-            emptyLeft = 0;
-            list.add(new PrinterInstruction(PrinterInstruction.Direction.FORWARD, 1));
         }
         return list;
     }
