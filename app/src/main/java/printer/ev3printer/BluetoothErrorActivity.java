ab913d041be1e44152eb9a1c54b73107aaa6277f
Simone Romanello <rommyalvise@hotmail.it>
2019-01-16 15:42:22 +0100

    working exit button

--- app/src/main/java/printer/ev3printer/BluetoothErrorActivity.java
+++ app/src/main/java/printer/ev3printer/BluetoothErrorActivity.java
@@ -1,7 +1,10 @@
 package printer.ev3printer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class BluetoothErrorActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class BluetoothErrorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_error);
+
+        Button exitButton = findViewById(R.id.BluetoothErrorActivityButton);
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+            }
+        });
     }
 }
