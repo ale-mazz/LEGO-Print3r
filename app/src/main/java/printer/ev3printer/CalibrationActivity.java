7bbbced9f3d98e815a2d564fceb0cde6aedff94f
Simone Romanello <rommyalvise@hotmail.it>
2019-01-25 17:13:46 +0100

    set calibration activity to portrait mode

--- app/src/main/java/printer/ev3printer/CalibrationActivity.java
+++ app/src/main/java/printer/ev3printer/CalibrationActivity.java
@@ -1,5 +1,6 @@
 package printer.ev3printer;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,6 @@ public class CalibrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calibration);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
